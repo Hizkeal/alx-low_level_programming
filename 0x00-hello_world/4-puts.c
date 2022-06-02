44294150d1a679dd0358224a3e82f4b6b327f1e4
Hizkeal <104999321+Hizkeal@users.noreply.github.com>
2022-06-02 23:04:16 +0300

    First code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - Write '"Programming is like building a multilingual puzzle'
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
