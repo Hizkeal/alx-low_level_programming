4bec482150acbe21955f0d5fb38df0a98fe347e9
Hizkeal <104999321+Hizkeal@users.noreply.github.com>
2022-06-07 21:14:37 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,15 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * _isalpha - checks if character is a letter
- *            both lowercase or uppercase
- *
- * @c: takes input from other functions.
- *
- * Return: 1 is c if true else 0
-*/
+ *_isalpha - checks for a alphabetic character using a function
+ *@c: letter being tested
+ * Return: Always 0 succesS
+ */
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
-		return (1);
-	return (0);
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+  return (1);
+else
+  return (0);
 }
-
